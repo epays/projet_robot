a1c107bebdbf87d6033ddf22c693d15236ccc808
epays <even.pays@gmail.com>
2017-03-06 14:54:30 +0100

    Update exo3.py

--- exo3.py
+++ exo3.py
@@ -14,12 +14,13 @@ mot2.run_timed(time_sp=3000, speed_sp=500)
 obs_detection = InfraredSensor()
 
 distance = 30
-gauche = LargeMotor(port=LargeMotor.PORT.B)
-droite = LargeMotor(port=LargeMotor.PORT.C)
+
 
 if obs_detection <= distance:
-            gauche.stop()
-            droite.stop()
+            mot1.stop()
+            mot2.stop()
+else
+pass
 
 
 
