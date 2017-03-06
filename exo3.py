adcced022704d913852e71796b9f10fb9107e01f
epays <even.pays@gmail.com>
2017-03-06 14:18:49 +0100

    Update exo3.py

--- exo3.py
+++ exo3.py
@@ -1,7 +1,10 @@
-import LargeMotor  from ev3.lego 
-import * from ev3dev.ev3 
-import InfraredSensor from ev3.lego 
+from ev3  import LargeMotor  
+from ev3dev  import * 
+from ev3 import *
+from ev3 import InfraredSensor 
 
+moteur = Motor(OUTPUT_A)
+moteur.run_timed(time_sp=3000, speed_sp=500)
 obs_detection = InfraredSensor()
 
 distance = 30
