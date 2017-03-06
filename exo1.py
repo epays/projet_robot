e86905d2d63b9fb975a1058af6f444ee09b9d799
epays <even.pays@gmail.com>
2017-03-06 15:07:48 +0100

    Update exo1.py

--- exo1.py
+++ exo1.py
@@ -1,16 +1,15 @@
 #! / usr / bin / env python3 
 import ev3dev.ev3 as ev3
 from ev3dev.ev3  import  *
-
-ev3.Sound.setLanguage("French") #définir la langue sur français
+# coding: utf-8
 
 
 cl = colorSENSOR () #définir le capteur comme capteur de couleur
 assert cl.connected, "Connecter un capteur de couleur à un port de capteur" #vérifier que le capteur est connecté sinon erreur
 cl.mode = 'COL-COLOR' #mettre le capteur cl en mode reconnaissance de couleur
-colors = ( "inconnu", "noir", "bleu", "vert", "vert", "rouge", "blanc", "marron")
+colors = ( 'inconnu', 'noir', 'bleu', 'vert', 'jaune', 'rouge', 'blanc', 'marron', lang='fr')
 if colors = "rouge":
-	ev3.Sound.speak('Au Revoir').exit() #si la couleur est rouge, le programme s'arrête
+	Sound.speak('Au Revoir', lang='fr').exit() #si la couleur est rouge, le programme s'arrête
 	pass
 else :
 	print(colors[cl.value()]) #affiche la couleur en chaine de caractère
