f90d761a453c4271aa3b90c4ff1729f8f5a415e2
epays <even.pays@gmail.com>
2017-03-06 14:30:41 +0100

    Update exo3.py

--- exo3.py
+++ exo3.py
@@ -3,8 +3,14 @@ from ev3dev  import *
 from ev3 import *
 from ev3 import InfraredSensor 
 
-moteur = Motor(OUTPUT_A)
-moteur.run_timed(time_sp=3000, speed_sp=500)
+mot1 = LargeMotor('outB')
+assert m1.connected, "Connecter un large motor sur outB"
+mot2 = LargeMotor('outC')
+assert m2.connected, "Connecter un large motor sur outC"
+
+mot1.run_timed(time_sp=3000, speed_sp=500)
+mot2.run_timed(time_sp=3000, speed_sp=500)
+
 obs_detection = InfraredSensor()
 
 distance = 30
