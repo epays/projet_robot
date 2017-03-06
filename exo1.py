02106a1500636b64179afd497882918de7c25a4a
epays <even.pays@gmail.com>
2017-03-06 13:03:59 +0100

    Add files via upload

--- exo1.py
+++ exo1.py
@@ -1,20 +1,17 @@
 #! / usr / bin / env python3 
 import ev3dev.ev3 as ev3
 from ev3dev.ev3  import  *
-import sys
 
-ev3.Sound.setLanguage("French")
+ev3.Sound.setLanguage("French") #définir la langue sur français
 
-# Detection des couleurs 
+
 cl = colorSENSOR () #définir le capteur comme capteur de couleur
 assert cl.connected, "Connecter un capteur de couleur à un port de capteur" #vérifier que le capteur est connecté sinon erreur
 cl.mode = 'COL-COLOR' #mettre le capteur cl en mode reconnaissance de couleur
-colors = ( "unknown", "black", "blue", "green", "yellow", "red", "white", "brown")
-if colors = "red":
+colors = ( "inconnu", "noir", "bleu", "vert", "vert", "rouge", "blanc", "marron")
+if colors = "rouge":
 	ev3.Sound.speak('Au Revoir').exit() #si la couleur est rouge, le programme s'arrête
 	pass
 else :
 	print(colors[cl.value()]) #affiche la couleur en chaine de caractère
     Sound.speak(colors[cl.value()]).wait() #dit la couleur
-
-
---
+++ exo2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+from ev3dev.ev3 import *
+from time       import sleep
+import sys
+
+def beeps( nb ):  #plusieurs bips
+   for i in range (0,nb):
+      Sound.beep()
+      sleep(.2)
+
+def exitrouge():
+   if cl.value(0) > 100:  # panneau rouge 
+      if cl.value(1) + cl.value(2) < 50:
+         m1.run_forever(speed_sp = 0)
+         m2.run_forever(speed_sp = 0)
+         beeps(3)
+         sleep(1)
+         sys.exit(0)
+
+m1 = LargeMotor('outB')
+assert m1.connected, "Connecter un large motor sur outB"
+m2 = LargeMotor('outC')
+assert m2.connected, "Connecter un large motor sur outC"
+
+beeps(1)
+
+ir = InfraredSensor()
+assert ir.connected, "Connecter le senseur infrarouge a un des ports"
+
+#Mettre le senseur infrarouge en mode de proximite
+ir.mode = 'IR-PROX'
+
+cl = ColorSensor() 
+assert cl.connected, "Connecter le senseur de couleur a un des ports"
+
+# Mettre le senseur en mode RGB
+cl.mode='RGB-RAW'
+
+cur_distance = ir.value()
+# print "Distance %d cur_distance de depart" % cur_distance
+
+m1.run_forever(speed_sp = 0)
+m2.run_forever(speed_sp = 0)
+
+while ir.value() > 2:
+    exitrouge()  #stoppe avec panneau rouge
+
+    cur_distance = ir.value()
+    # print "Distance1 %d cur_distance" % cur_distance
+
+    while ir.value() > 7:
+       exitrouge()
+
+       cur_distance = ir.value()
+       # print "Distance2 %d cur_distance" % cur_distance
+       m1.run_forever(speed_sp = 300 - 2000/(cur_distance + 3))
+       m2.run_forever(speed_sp = 300 - 2000/(cur_distance + 3))
+
+    m1.run_forever(speed_sp = 0)
+    m2.run_forever(speed_sp = 0)
+    m1.run_forever(speed_sp = -400)
+    m2.run_forever(speed_sp = -400)
+    sleep(1.5)
+
+    exitrouge()  #stoppe avec panneau rouge
+
+    for num in range(1,5):
+        m2.run_timed(speed_sp=400)
+        sleep(0.6)
+        exitrouge()  #stoppe avec panneau rouge
+
+        if ir.value() < 7:
+            break
+
+m1.run_forever(speed_sp = 0)
+m2.run_forever(speed_sp = 0)
+
+beeps(2)
+sleep(1)
