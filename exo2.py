bf95edadb09ea08a4969bfc4f7d71f8fce9ca71f
epays <even.pays@gmail.com>
2017-03-07 15:25:09 +0100

    Update exo2.py

--- exo2.py
+++ exo2.py
@@ -1,79 +1,35 @@
 #!/usr/bin/env python3
-import * from ev3dev.ev3 
-import sleep from time       
+from ev3dev.ev3 import *
+import ev3dev.ev3 as ev3
+from time       import sleep
 import sys
 
-def beeps( nb ):  #plusieurs bips
-   for i in range (0,nb):
-      Sound.beep()
-      sleep(.2)
-
-def exitrouge():
-   if cl.value(0) > 100:  # panneau rouge 
-      if cl.value(1) + cl.value(2) < 50:
-         m1.run_forever(speed_sp = 0)
-         m2.run_forever(speed_sp = 0)
-         beeps(3)
-         sleep(1)
-         sys.exit(0)
-
-m1 = LargeMotor('outB')
+m1 = ev3.LargeMotor('outB')
 assert m1.connected, "Connecter un large motor sur outB"
-m2 = LargeMotor('outC')
+m2 = ev3.LargeMotor('outC')
 assert m2.connected, "Connecter un large motor sur outC"
 
-beeps(1)
-
 ir = InfraredSensor()
-assert ir.connected, "Connecter le senseur infrarouge a un des ports"
+assert ir.connected, "Connecter svp le senseur infrarouge a un des ports"
 
 #Mettre le senseur infrarouge en mode de proximite
 ir.mode = 'IR-PROX'
 
-cl = ColorSensor() 
-assert cl.connected, "Connecter le senseur de couleur a un des ports"
-
-# Mettre le senseur en mode RGB
-cl.mode='RGB-RAW'
-
 cur_distance = ir.value()
-# print "Distance %d cur_distance de depart" % cur_distance
 
-m1.run_forever(speed_sp = 0)
-m2.run_forever(speed_sp = 0)
+m1.run_forever(speed_sp = 400) #Les moteurs avancent en permanence
+m2.run_forever(speed_sp = 400)
 
-while ir.value() > 2:
-    exitrouge()  #stoppe avec panneau rouge
-
-    cur_distance = ir.value()
-    # print "Distance1 %d cur_distance" % cur_distance
-
-    while ir.value() > 7:
-       exitrouge()
-
-       cur_distance = ir.value()
-       # print "Distance2 %d cur_distance" % cur_distance
-       m1.run_forever(speed_sp = 300 - 2000/(cur_distance + 3))
-       m2.run_forever(speed_sp = 300 - 2000/(cur_distance + 3))
-
-    m1.run_forever(speed_sp = 0)
-    m2.run_forever(speed_sp = 0)
-    m1.run_forever(speed_sp = -400)
-    m2.run_forever(speed_sp = -400)
-    sleep(1.5)
-
-    exitrouge()  #stoppe avec panneau rouge
-
-    for num in range(1,5):
-        m2.run_timed(speed_sp=400)
-        sleep(0.6)
-        exitrouge()  #stoppe avec panneau rouge
-
-        if ir.value() < 7:
-            break
-
-m1.run_forever(speed_sp = 0)
-m2.run_forever(speed_sp = 0)
-
-beeps(2)
-sleep(1)
+if cur_distance =< 5: #Si la distance entre lobstacle et la machine est inférieure ou égale à 5
+    m1.stop() #Les moteurs s'arrêtent
+    m2.stop()
+    m1.run_timed(time_sp=2000, speed_sp=-400)
+    m2.run_timed(time_sp=2000, speed_sp=-400)
+    sleep(3)
+    m1.run_to_rel_pos(position_sp=45, speed_sp=400, stop_action="hold") # Le moteur 1 tourne à 45 degrées
+    sleep(3)
+    m1.run_forever(speed_sp = 400)
+    m2.run_forever(speed_sp = 400)
+else:
+    m1.run_forever(speed_sp = 400)#Sinon les moteurs continuet d'avancer
+    m2.run_forever(speed_sp = 400)
