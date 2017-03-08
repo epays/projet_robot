ef8749a76cfaff2c42d2cddda60c132f48064f4d
epays <even.pays@gmail.com>
2017-03-08 14:51:42 +0100

    Update lance-balle.py

--- lance-balle.py
+++ lance-balle.py
@@ -5,43 +5,17 @@ from ev3dev.ev3 import *
 from ev3.ev3dev import InfraredSensor()
 from ev3.ev3dev import TouchSensor()
 import os, sys
-import time
+import time     
 
-
-
-
-
-        # Connecte les équipements nécessaires et on vérifie
-class Patator:
-    def __init__(self):
+class RobotTir:
+        def __init__(self):
         self.mm = ev3.MediumMotor()
-        self.ir = ev3.InfraredSensor()
-        self.ts = ev3.TouchSensor()
-
-        check(self.mm.connected, 'Connecter le medium motor !')
-
-        check(self.ir.connected, 'Connecter le Infrared Sensor !')
-        check(self.ts.connected, 'Connecter le touch sensor !')
-
-        # Reset des moteurs
-        for m in (self.ir, self.ts, self.mm):
-            m.reset()
-            m.position = 0
-            m.stop_action = 'brake'
-
-       
-
-    class Shooter(TRACK3R):
-
-    def __init__(self, medium_motor=OUTPUT_A):
-        TRACK3R.__init__(self, medium_motor)
-        self.remote.on_beacon = self.fire_ball
-
-    def fire_ball(self, state):
-        if state:
-            self.medium_motor.run_to_rel_pos(speed_sp=400, position_sp=3*360)
-        else:
-self.medium_motor.stop()
+        check(self.mm.connected, 'Connecter le medium motor sur port A!')
+  
+shot = 0        
+    while shot < 4:
+        self.mm.run_to_rel_pos(speed_sp=900, position_sp=(-1080 if direction == 'up' else 1080))
+        shot = shot +1
 
 
 
