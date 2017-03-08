5cbfdac2139172e7d0a9b6acbea12da8c001c6ac
epays <even.pays@gmail.com>
2017-03-08 14:47:30 +0100

    Update comat_robot.py

--- comat_robot.py
+++ comat_robot.py
@@ -9,46 +9,29 @@ import os, sys
 import time, random
 
 obs_detection = ev3.InfraredSensor()
-obs_detection.mode = 'IR-DIST-CM'
-distance = 30
+obs_detection.mode = 'IR-PROX'
 
-class robotPls:
+
+class RobotPls:
     def __init__(self):
         self.lm = ev3.LargeMotor()
-        self.rm = ev3.MediumMotor()
+        self.rm = ev3.LargeMotor()
         self.mm = ev3.MediumMotor()
         self.ir = ev3.InfraredSensor()
-        self.ts = ev3.TouchSensor()
 
-        check(self.lm.connected, 'Connecter le large motor gauche !')
-        check(self.rm.connected, 'Connecter le large motor droit !')
-        check(self.mm.connected, 'Connecter le medium motor !')
+        check(self.lm.connected, 'Connecter le large motor gauche sur port B !')
+        check(self.rm.connected, 'Connecter le large motor droit sur port C !')
+        check(self.mm.connected, 'Connecter le medium motor sur port A!')
         check(self.ir.connected, 'Connecter le Infrared Sensor !')
-        check(self.ts.connected, 'Connecter le touch sensor !')
-
-        # Reset des moteurs
-        for m in (self.ir, self.ts, self.mm, self.lm, self.rm):
-            m.reset()
-            m.position = 0
-            m.stop_action = 'brake'
 
 
-
-
-while obs_detection.value >= distance:
-    lm.run_forever(speed_sp=500)
-    rm.run_forever(speed_sp=500)
-else      
-    def shoot(self, direction='up'):
-           
-       # Lance une balle
-       self.mm.run_to_rel_pos(speed_sp=900, position_sp=(-1080 if direction == 'up' else 1080))
-    while 'running' in self.mm.state:
-            time.sleep(0.1)
-
-
-
-    def shoot(direction):
-    def on_press(state):
-            if state: self.shoot(direction)
-    return 0
+    while obs_detection.value >= 30:
+        self.lm.run_forever(speed_sp=500)
+        self.rm.run_forever(speed_sp=500)     
+shot = 0        
+    while obs_detection.value <= 30 && shot < 4:
+        self.lm.stop()
+        self.rm.stop()
+        self.mm.run_to_rel_pos(speed_sp=900, position_sp=(-1080 if direction == 'up' else 1080))
+        shot = shot +1
+        
