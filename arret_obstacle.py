33b5270e2ed9ce29779920d14773a5254f087769
epays <even.pays@gmail.com>
2017-03-08 16:55:39 +0100

    Update arret_obstacle.py

--- arret_obstacle.py
+++ arret_obstacle.py
@@ -1,8 +1,7 @@
 import ev3dev.ev3 as ev3
-from ev3 import LargeMotor 
 from ev3dev.ev3 import *
-from ev3.ev3dev import InfraredSensor()
 import os, sys
+
 obs_detection = ev3.InfraredSensor()
 obs_detection.mode = 'IR-PROX'
 distance = 30
@@ -14,10 +13,6 @@ assert m2.connected, "Connecter un large motor sur outC"
 
 m1.run_forever(speed_sp=500)
 m2.run_forever(speed_sp=500)
-
-'''if obs_detection.value <= distance:
-            m1.stop()
-            m2.stop()'''
 
 while obs_detection.value > distance: 
 	m1.run_forever(speed_sp=500)
