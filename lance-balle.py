c8084613a01be3d77e45aa765447e832552044ad
navatard <mathieu.berchi@gmail.com>
2017-03-08 16:36:07 +0100

    Update lance-balle.py

--- lance-balle.py
+++ lance-balle.py
@@ -1,21 +1,13 @@
 #!/usr/bin/env python3
 import ev3dev.ev3 as ev3
-from ev3 import MediumMotor()
 from ev3dev.ev3 import *
-from ev3.ev3dev import InfraredSensor()
-from ev3.ev3dev import TouchSensor()
-import os, sys
+import os,sys
 import time     
 
-class RobotTir:
-        def __init__(self):
-        self.mm = ev3.MediumMotor()
-        check(self.mm.connected, 'Connecter le medium motor sur port A!')
-  
+mm = ev3.MediumMotor()
+assert mm.connected, 'Connecter le medium motor sur port A!'
 shot = 0        
-    while shot < 4:
-        self.mm.run_to_rel_pos(speed_sp=900, position_sp=(-1080 if direction == 'up' else 1080))
-        shot = shot +1
-
-
+while shot <= 4:
+        mm.run_to_rel_pos(speed_sp=900, position_sp=(-1080 if direction == 'up' else 1080))
+        shot += 1
 
