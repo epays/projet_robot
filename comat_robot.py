ee259f8b60d1be44c9c3e0c43c355be99b1af685
epays <even.pays@gmail.com>
2017-03-08 16:37:18 +0100

    Update comat_robot.py

--- comat_robot.py
+++ comat_robot.py
@@ -3,8 +3,8 @@ import ev3dev.ev3 as ev3
 from ev3 import MediumMotor
 from ev3 import LargeMotor
 from ev3dev.ev3 import *
-from ev3.ev3dev import InfraredSensor
-from ev3.ev3dev import TouchSensor
+from ev3dev.ev3 import InfraredSensor
+from ev3dev.ev3 import TouchSensor
 import os, sys
 import time, random
 
@@ -12,26 +12,25 @@ obs_detection = ev3.InfraredSensor()
 obs_detection.mode = 'IR-PROX'
 
 
-class RobotPls:
-    def __init__(self):
-        self.lm = ev3.LargeMotor()
-        self.rm = ev3.LargeMotor()
-        self.mm = ev3.MediumMotor()
-        self.ir = ev3.InfraredSensor()
+        lm = ev3.LargeMotor('outB')
+        assert lm.connected, "Connecter un large motor sur outB"
+        rm = ev3.LargeMotor('outC')
+        assert rm.connected, "Connecter un large motor sur outC"
+        mm = ev3.MediumMotor('outA')
+        assert mm.connected, "Connecter un medium motor sur outA"
+        ir = ev3.InfraredSensor('outD')
+        assert ir.connected, "Connecter un infrared sensor sur outD"
 
-        check(self.lm.connected, 'Connecter le large motor gauche sur port B !')
-        check(self.rm.connected, 'Connecter le large motor droit sur port C !')
-        check(self.mm.connected, 'Connecter le medium motor sur port A!')
-        check(self.ir.connected, 'Connecter le Infrared Sensor !')
+
 
 
     while obs_detection.value >= 30:
-        self.lm.run_forever(speed_sp=500)
-        self.rm.run_forever(speed_sp=500)     
+        lm.run_forever(speed_sp=500)
+        rm.run_forever(speed_sp=500)     
 shot = 0        
     while obs_detection.value <= 30 && shot < 4:
-        self.lm.stop()
-        self.rm.stop()
-        self.mm.run_to_rel_pos(speed_sp=900, position_sp=(-1080 if direction == 'up' else 1080))
+        lm.stop()
+        rm.stop()
+        mm.run_to_rel_pos(speed_sp=900, position_sp=(-1080 if direction == 'up' else 1080))
         shot = shot +1
         
